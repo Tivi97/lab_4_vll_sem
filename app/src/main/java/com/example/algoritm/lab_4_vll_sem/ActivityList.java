8dab85a4481fb177b282d973954942f6369348e7
Alevtina_tivi <alevtina97tivi@gmail.com>
2017-12-25 21:04:37 +0400

    25.12.17

--- app/src/main/java/com/example/algoritm/lab_4_vll_sem/ActivityList.java
+++ app/src/main/java/com/example/algoritm/lab_4_vll_sem/ActivityList.java
@@ -1,26 +1,53 @@
 package com.example.algoritm.lab_4_vll_sem;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
 
 /**
  * Created by Algoritm on 24.12.2017.
  */
 
 public class ActivityList extends AppCompatActivity{
+
+    String albumNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
+
     }
+
+
     public void openActivityPlayer(View view){
 
         Intent intent1 = new Intent(ActivityList.this, ActivityPlayer.class);
         startActivity(intent1);
 
+          }
+
+    public void mess(View view){
+        switch (view.getId()){
+            case R.id.imageBND: albumNumber="3";
+                break;
+            case R.id.imageB25: albumNumber="2";
+                break;
+            case R.id.imageSLA: albumNumber="1";
+                break;
+        }
+        Intent constructor = new Intent(ActivityList.this, ActivityPlayer.class);
+        constructor.putExtra(ActivityPlayer.NUMBER_OF_ALBUM, albumNumber);
+        startActivity(constructor);
+
+        Intent intent1 = new Intent(ActivityList.this, ActivityPlayer.class);
+        startActivity(intent1);
     }
+
+
 
 }
--- app/src/main/java/com/example/algoritm/lab_4_vll_sem/ActivityPlayer.java
+++ app/src/main/java/com/example/algoritm/lab_4_vll_sem/ActivityPlayer.java
@@ -1,9 +1,18 @@
 package com.example.algoritm.lab_4_vll_sem;
 
+import android.app.Fragment;
+import android.app.ListFragment;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 /**
  * Created by Algoritm on 17.12.2017.
@@ -12,11 +21,34 @@ import android.view.View;
 public class ActivityPlayer  extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+    static String NUMBER_OF_ALBUM;
+
+    String BestOf25Years[] = new String[] { "All this time", "Englishman in New York" };
+    String SacredLove[] = new String[] { "I`m so happy i can`t stop crying", "It`s probably me" };
+    String BrandNewDay[] = new String[] { "Desert Rose", "Mad about you"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        ArrayList<String> songNames = new ArrayList<>();
+
+
+        Intent p=getIntent();
+        String messageText = p.getStringExtra(NUMBER_OF_ALBUM);
+        switch (messageText){
+            case "1": songNames.add(0,SacredLove.toString());
+            case "2": songNames.add(0,BestOf25Years.toString());
+            case "3": songNames.add(0,BrandNewDay.toString());
+        }
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, songNames);
+        listView.setAdapter(adapter);
+
 
         findViewById(R.id.playButton).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,4 +84,6 @@ public class ActivityPlayer  extends AppCompatActivity {
         mediaPlayer.release();
     }
 
+
 }
+
